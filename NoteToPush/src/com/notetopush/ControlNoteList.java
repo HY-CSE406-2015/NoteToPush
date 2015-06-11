5ed3fe4c21b154d9cf9a5493ed4b8c7515b177ba
angrypeng@github.com <eookog@gmail.com>
2015-06-11 23:23:15 +0900

    delete 까지 구현하였..추후 DB작업 예정

--- NoteToPush/src/com/notetopush/ControlNoteList.java
+++ NoteToPush/src/com/notetopush/ControlNoteList.java
@@ -1,45 +1,63 @@
 package com.notetopush;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.*;
 
-
-
 public class ControlNoteList extends Activity {
-	
+
 	Button noteAddbtn;
-	View addedView;
-	RelativeLayout scrolllayout;
-	LinearLayout scrolllinear;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_control_note_list);
-        
-        noteAddbtn = (Button) findViewById(R.id.noteadd);
-        
-        noteAddbtn.setOnClickListener(new View.OnClickListener() {
-			
+	Button noteDeletebtn;
+	LinearLayout container;
+	TextView noteTitle;
+	String title;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_control_note_list);
+
+		noteAddbtn = (Button) findViewById(R.id.noteadd);
+		container = (LinearLayout) findViewById(R.id.container);
+		
+		noteAddbtn.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v) {
+			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				scrolllayout = (RelativeLayout)View.inflate(ControlNoteList.this, R.layout.eachnotelistlayout, null);
-				scrolllinear = (LinearLayout)findViewById(R.id.scrolllinearview);
-				if(scrolllinear.getChildCount()%2 == 0){
-					scrolllayout.setBackgroundColor(Color.GRAY);
+				LayoutInflater layoutInflater = 
+						(LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				final View eachNoteList = layoutInflater.inflate(R.layout.eachnotelistlayout,null);
+				noteTitle = (TextView)eachNoteList.findViewById(R.id.notetitle);
+				title = "note" + container.getChildCount(); //나중에 디비에서 받아와서 제목으로 하기
+				
+				noteDeletebtn = (Button)eachNoteList.findViewById(R.id.deletebtn);
+				noteDeletebtn.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						((LinearLayout)eachNoteList.getParent()).removeView(eachNoteList);
+					}
+				});
+				
+				if(container.getChildCount()%2 == 0){
+					eachNoteList.setBackgroundColor(Color.GRAY);
+					noteTitle.setText(title);
 				}
 				else{
-					scrolllayout.setBackgroundColor(Color.WHITE);
+					eachNoteList.setBackgroundColor(Color.WHITE);
+					noteTitle.setText(title);
 				}
-				scrolllinear.addView(scrolllayout);
+
+				container.addView(eachNoteList);
 			}
 		});
-    
-    }
+
+	}
 }
