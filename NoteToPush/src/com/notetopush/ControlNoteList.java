f0f6290254120f3261a278337f93ff7ff97a5cf1
angrypeng@github.com <eookog@gmail.com>
2015-06-15 03:46:40 +0900

    NoteList & ControlNoteList

--- NoteToPush/src/com/notetopush/ControlNoteList.java
+++ NoteToPush/src/com/notetopush/ControlNoteList.java
@@ -6,59 +6,69 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class ControlNoteList extends Activity implements ViewNoteList.ViewListener{
 
-   private ViewNoteList view;
-   private NoteList note_list;
-   private boolean is_created;
+	private ViewNoteList view;
+	private NoteList note_list;
+	private boolean is_created;
 
-   String title;
-   private ArrayList<String> noteTitle;
-   private ArrayList<Integer> note_id;
-   private ArrayList<Integer> note_noti_id;
-   private int noteIndex;
-   
-   @Override
-   protected void onCreate(Bundle savedInstanceState){
-      super.onCreate(savedInstanceState);
-      
-      this.note_list = new NoteList(this);
-      this.view = new ViewNoteList(this);
-      this.view.setListener(this);
-      setView();
-      setContentView(this.view.getView());
-      is_created = false;
-   }
-   protected void onResume(){
-      super.onResume();
-      if(is_created){
-         this.note_list = new NoteList(this);
-         setView();
-         setContentView(this.view.getView());
-      }
-      is_created = true;
-   }
+	String title;
+	private ArrayList<String> noteTitle;
+	private ArrayList<Integer> note_id;
+	private ArrayList<Integer> note_noti_id;
+	private int noteIndex;
 
-   private void setView(){
-      for(int list_loop=0; list_loop<this.note_list.getSize();list_loop++){
-         view.addNoteContainer(note_list.getNoteTitles(list_loop), note_list.getNoteId(list_loop));
-      }
-   }
-   public void addButtonAction() {
-      Intent i = new Intent(ControlNoteList.this, ControlNoteEdit.class);
-      startActivity(i);
-   }
-   public void noteClickAction(int note_id){
-      Intent i = new Intent(ControlNoteList.this, ControlNoteEdit.class);
-      i.putExtra(ControlNoteContent.CONTROL_NOTE_ID, note_id);
-      startActivity(i);
-   }
-   public void deleteButtonAction(int note_id, View note){
-      //삭제 확인 팝업 추가
-      note_list.deleteNote(note_id);
-      view.deleteNote(note);
-   }
-   
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+
+		Log.i("ControlNoteList","Start make NoteList");
+		this.note_list = new NoteList(this);
+		Log.i("ControlNoteList","Start make ViewNoteList");
+		this.view = new ViewNoteList(this);
+		Log.i("ControlNoteList","Start make NoteList.setListener");
+		this.view.setListener(this);
+		Log.i("ControlNoteList","Start setView");
+		setView();
+		Log.i("ControlNoteList","Start setContent");
+		setContentView(this.view.getView());
+		is_created = false;
+	}
+	protected void onResume(){
+		super.onResume();
+		if(is_created){
+			this.note_list = new NoteList(this);
+			setView();
+			setContentView(this.view.getView());
+		}
+		is_created = true;
+	}
+
+	private void setView(){
+		Log.i("CtlNL",""+this.note_list.getSize());
+		for(int list_loop=0; list_loop<this.note_list.getSize();list_loop++){
+			Log.i("CtlNL",""+list_loop);
+			view.addNoteContainer(note_list.getNoteTitles(list_loop), note_list.getNoteId(list_loop));
+		}
+	}
+	public void addButtonAction() {
+		Intent i = new Intent(ControlNoteList.this, ControlNoteEdit.class);
+		startActivity(i);
+	}
+	public void noteClickAction(int note_id){
+		Intent i = new Intent(ControlNoteList.this, ControlNoteEdit.class);
+		//i.putExtra(ControlNoteContent.CONTROL_NOTE_ID, note_id);
+		Toast.makeText(this, "토스트메시지입니다.", Toast.LENGTH_SHORT).show();
+		startActivity(i);
+	}
+	public void deleteButtonAction(int note_id, View note){
+		//삭제 확인 팝업 추가
+		note_list.deleteNote(note_id);
+		view.deleteNote(note);
+	}
+
 }
--- NoteToPush/src/com/notetopush/ViewNoteList.java
+++ NoteToPush/src/com/notetopush/ViewNoteList.java
@@ -2,11 +2,13 @@
 package com.notetopush;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ViewNoteList{
 
@@ -39,27 +41,46 @@ public class ViewNoteList{
     this.listen_controller = listener;
   }
   public void addNoteContainer(String title, int note_id){
+	Log.i("addNoteCon","1");
     View note = View.inflate(this.context, R.layout.eachnotelistlayout, null);
-    EditText noteId = (EditText)note.findViewById(R.id.editText1);
-    noteId.setText(title);
+//    
+//	EditText noteId = (EditText)note.findViewById(R.id.editText1);
+//    noteId.setText(title);
+    Log.i("addNoteCon","2");
     TextView note_title = (TextView)note.findViewById(R.id.notetitle);
+    Log.i("addNoteCon","2.1 //"+note_title.getId() );
     note_title.setText(title);
-    note_title.setTag(0, new Integer(note_id));
+    Log.i("addNoteCon","2.2");
+    //note_title.setTag(0, new Integer(note_id));
+    note_title.setTag(R.id.notetitle,note_id);
+    Log.i("addNoteCon","3");
     Button delete_btn = (Button)note.findViewById(R.id.deletebtn);
-    delete_btn.setTag(0, new Integer(note_id));
-    delete_btn.setTag(1, note);
+    //delete_btn.setTag(0, new Integer(note_id));
+    delete_btn.setTag(R.id.notetitle,note_id);
+    //delete_btn.setTag(1, note);
+    delete_btn.setTag(R.id.deletebtn,note);
+    Log.i("addNoteCon","4");
+
     View.OnClickListener note_listener = new View.OnClickListener() {
       public void onClick(View v) {
         int id = v.getId();
-        int note_id = (Integer)v.getTag(0);
+        int note_id = (Integer) v.getTag(R.id.notetitle);
         if(id == R.id.notetitle){
-          listen_controller.noteClickAction(note_id);
+          //listen_controller.noteClickAction(note_id);
+          Log.i("Click","Title = "+note_id);
         }else if(id == R.id.deletebtn){
-          listen_controller.deleteButtonAction(note_id, (View)v.getTag(1));
+          //listen_controller.deleteButtonAction(note_id, (View)v.getTag(1));
+          //listen_controller.deleteButtonAction(note_id, (View)v.getTag(R.id.deletebtn));
+          Log.i("Click","Delete Title = "+note_id);
         }
       }
     };
+    note_title.setOnClickListener(note_listener);
+    delete_btn.setOnClickListener(note_listener);
+    
+    Log.i("addNoteCon","5");
     this.container.addView(note);
+    Log.i("addNoteCon","6");
   }
   public void deleteNote(View note){
     this.container.removeView(note);
